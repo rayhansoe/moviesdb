ed99fe69deb64a69f9ff7818f8c30e25f7ca7f43
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-11 19:30:28 +0700

    add logic, state and function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import "./App.css"
+import React, { lazy, useState, Suspense, useEffect, useRef } from "react"
+// import { delays } from "./tools/delays"
+const Header = lazy(() => import("./components/Header"))
+const SearchBox = lazy(() => import("./components/SearchBox"))
 
 function App() {
+	const [title, setTitle] = useState("Avengers")
+	const [movies, setMovies] = useState([])
+	const _title = useRef("")
+
+	useEffect(() => {
+		const a = fetch(`https://www.omdbapi.com/?s=${title}&apikey=41eec44f`)
+		a.then(res => res.json()).then(data => setMovies(() => data.Search))
+	}, [title])
+
+	const handleChange = e => {
+		_title.current = e
+	}
+
 	return (
 		<>
 			<div className='container'>
-				<header className='nav-bg'>
-					<nav className='navbar'>
-						<h1 className='logo'>MoviesDB</h1>
-						<ul className='menu'>
-							<li>
-								<a href='#' className='active'>
-									Search Movie
-								</a>
-							</li>
-							<li>
-								<a href='#'>Top IMDb</a>
-							</li>
-						</ul>
-					</nav>
-				</header>
+				<Suspense fallback={<h1>Loading.... </h1>}>
+					<Header />
+				</Suspense>
+
 				<div className='separator'></div>
-				<input type='text' className='search-box' />
-				<h1 className='title'>Avengers</h1>
+
+				<Suspense fallback={<h1>Loading.... </h1>}>
+					<SearchBox onChange={handleChange} setTitle={setTitle} preTitle={_title} />
+				</Suspense>
+
+				<h1 className='title'>{title}</h1>
+				<div className='movies-list'>
+					{movies.length === 0
+						? []
+						: movies.map(movie => {
+								return (
+									<div className='movie' key={movie.imdbID}>
+										<img src={movie.Poster} alt='' />
+									</div>
+								)
+						  })}
+				</div>
+				<div className='separator' style={{ backgroundColor: "#ffffff" }}></div>
 			</div>
 		</>
 	)
