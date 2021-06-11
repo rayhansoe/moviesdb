6161fc4b9e018c7b1b547e8680b8151ce950e267
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-11 19:29:48 +0700

    search box component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,23 @@
+import { delays } from "../tools/delays"
+import React, { useMemo } from "react"
+
+const SearchBox = ({ onChange, setTitle, preTitle }) => {
+	const delay = useMemo(
+		() => delays(() => setTitle(() => preTitle.current), 1000),
+		[preTitle, setTitle]
+	)
+
+	return (
+		<div>
+			<input
+				type='text'
+				className='search-box'
+				onChange={e => onChange(e.target.value)}
+				onKeyUp={delay}
+				placeholder={"Avengers"}
+			/>
+		</div>
+	)
+}
+
+export default SearchBox
