3c0972b308e4dac8b53a76ac0241e4474fb8b2d8
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-20 20:13:41 +0700

    add new condition when fail fetch / movie not found.

--- src/components/SearchResultsDetails.jsx
+++ src/components/SearchResultsDetails.jsx
@@ -1,21 +1,35 @@
 const SearchResultsDetails = ({ title, totalResults }) => {
 	const renderSearchResultsDetails = () => {
-		return (
-			title &&
-			totalResults.length !== 0 && (
+		if (title && totalResults) {
+			return (
 				<>
 					<h1 className='title'>{title}</h1>
-
 					<p>
 						Explore <span>{totalResults}</span> movie information, all movie content and movie
 						images from OMDb API.
 					</p>
 				</>
 			)
-		)
+		} else if (title && totalResults === 0) {
+			return (
+				<>
+					<h1 className='title'>{title}</h1>
+
+					<p>
+						We could not find anything for "sdadawdaw".
+						<br />
+						Search result: <span>{totalResults}</span>
+					</p>
+				</>
+			)
+		}
 	}
 
-	return <div className='search-results-details'>{renderSearchResultsDetails()}</div>
+	return (
+		<div className='search-results-details' style={{ marginBottom: totalResults ? "60px" : "0px" }}>
+			{renderSearchResultsDetails()}
+		</div>
+	)
 }
 
 export default SearchResultsDetails
