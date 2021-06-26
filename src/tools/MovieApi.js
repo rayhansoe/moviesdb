446b71f0c9e27633c3f3b72e85bce4365bfc8d58
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-26 19:38:02 +0700

    add new functions -> (getMovieByImdbID, getMovieVideos and getMovieTrailers), and add new init variables.

--- src/tools/MovieApi.js
+++ src/tools/MovieApi.js
@@ -1,5 +1,7 @@
 import { handleError, handleResponse } from "./apiUtils"
 let baseUrl
+let movieID
+let Trailers
 
 export const getMovie = async imdbID => {
 	if (imdbID) {
@@ -11,4 +13,41 @@ export const getMovie = async imdbID => {
 			return handleError(error)
 		}
 	}
+}
+
+export const getMovieByImdbID = async imdbID => {
+	if (imdbID) {
+		baseUrl = `https://api.themoviedb.org/3/find/${imdbID}?api_key=f363fbafab56237920b96af2c295f5e1&external_source=imdb_id`
+		try {
+			const response = await fetch(baseUrl)
+			return handleResponse(response)
+		} catch (error) {
+			return handleError(error)
+		}
+	}
+}
+
+export const getMovieVideos = async imdbID => {
+	if (imdbID) {
+		movieID = await getMovieByImdbID(imdbID).then(res => res.movie_results[0]?.id)
+		if (movieID) {
+			baseUrl = `https://api.themoviedb.org/3/movie/${movieID}/videos?api_key=f363fbafab56237920b96af2c295f5e1&language=en-US`
+			try {
+				const response = await fetch(baseUrl)
+				return handleResponse(response)
+			} catch (error) {
+				return handleError(error)
+			}
+		}
+	}
+	return
+}
+
+export const getMovieTrailers = async imdbID => {
+	if (imdbID) {
+		Trailers = await getMovieVideos(imdbID).then(res => res?.results)
+		Trailers = Trailers.filter(vid => vid.type === "Trailer")
+		return Trailers
+	}
+	return
 }
