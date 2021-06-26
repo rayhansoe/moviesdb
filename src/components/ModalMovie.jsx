6b4fd1350ac7b0dd00c46c947ca1fdbf4d2002ba
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-26 19:39:08 +0700

    add useEffect for fetch the movie trailer and implement the movie trailer on jsx.

--- src/components/ModalMovie.jsx
+++ src/components/ModalMovie.jsx
@@ -1,13 +1,36 @@
 import { createPortal } from "react-dom"
+import React, { useMemo, useEffect, useRef, useState } from "react"
+import { getMovieTrailers } from "../tools/MovieApi"
 
-const ModalMovie = ({ open, onClose, movie }) => {
+const ModalMovie = ({ open, onClose, imdbID }) => {
+	const [movie, setMovie] = useState(() => {})
+	const movieTrailers = useRef(() => null)
+
+	// const getMovieAPI = useMemo(() => getMovieByID, [])
+	const getMovieAPI = useMemo(() => getMovieTrailers, [])
+
+	useEffect(() => {
+		if (imdbID) {
+			getMovieAPI(imdbID).then(res => {
+				if (res) {
+					movieTrailers.current = res
+					setMovie(() => `https://www.youtube.com/embed/${movieTrailers?.current[0]?.key}`)
+				}
+				return
+			})
+		}
+		return
+		// console.log(getMovieAPI(imdbID))
+	}, [getMovieAPI, imdbID, movieTrailers])
+
 	return createPortal(
 		<>
 			<div className={open ? "modal-movie visible" : "modal-movie"}>
 				<div className='layer' onClick={onClose}></div>
-				<div className='modal-content' onClick={() => console.log(movie)}>
+				<div className='modal-content'>
 					<div className='container'>
-						<h1>Haiii</h1>
+						{/* need to fix CORS or something... */}
+						<iframe width='1080' height='620' title='avengers' src={movie} allowFullScreen></iframe>
 					</div>
 				</div>
 			</div>
