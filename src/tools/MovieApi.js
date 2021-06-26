dd01aa1119175d1d322afc701bd4242bdcc189b7
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-26 15:30:41 +0700

    fetch async.

--- src/tools/MovieApi.js
+++ src/tools/MovieApi.js
@@ -1,9 +1,14 @@
 import { handleError, handleResponse } from "./apiUtils"
 let baseUrl
 
-export const getMovie = imdbID => {
+export const getMovie = async imdbID => {
 	if (imdbID) {
 		baseUrl = `https://www.omdbapi.com/?i=${imdbID}&apikey=41eec44f`
-		return fetch(baseUrl).then(handleResponse).catch(handleError)
+		try {
+			const response = await fetch(baseUrl)
+			return handleResponse(response)
+		} catch (error) {
+			return handleError(error)
+		}
 	}
 }
