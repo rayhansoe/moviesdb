9a7ea6ddea686c5d1f20f4415bf6a12296e9045d
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-06-30 07:24:44 +0700

    fix useEffect, add new state, rename prop + function, and add new conditional rendering.

--- src/components/ModalMovie.jsx
+++ src/components/ModalMovie.jsx
@@ -1,39 +1,62 @@
 import { createPortal } from "react-dom"
 import React, { useMemo, useEffect, useRef, useState } from "react"
-import { getMovieTrailers } from "../tools/MovieApi"
+import { getMovieTrailers, getMovieById } from "../tools/MovieApi"
 
-const ModalMovie = ({ open, onClose, imdbID }) => {
+const ModalMovie = ({ open, onClose, movieId }) => {
 	const [movie, setMovie] = useState(() => {})
-	const movieTrailers = useRef(() => null)
+	const [movieTrailers, setMovieTrailers] = useState(() => {})
+	const refMovieTrailers = useRef(() => null)
 
-	// const getMovieAPI = useMemo(() => getMovieByID, [])
-	const getMovieAPI = useMemo(() => getMovieTrailers, [])
+	const getMovieVideos = useMemo(() => getMovieTrailers, [])
+	const getMovie = useMemo(() => getMovieById, [])
 
 	useEffect(() => {
-		if (imdbID) {
-			getMovieAPI(imdbID).then(res => {
+		if (open) {
+			getMovieVideos(movieId).then(res => {
 				if (res) {
-					movieTrailers.current = res
-					setMovie(() => `https://www.youtube.com/embed/${movieTrailers?.current[0]?.key}`)
+					refMovieTrailers.current = res
+					setMovieTrailers(
+						() => `https://www.youtube.com/embed/${refMovieTrailers?.current[0]?.key}`
+					)
+				}
+				return
+			})
+			getMovie(movieId).then(res => {
+				if (res) {
+					setMovie(() => res)
 				}
 				return
 			})
 		}
 		return
-		// console.log(getMovieAPI(imdbID))
-	}, [getMovieAPI, imdbID, movieTrailers])
+	}, [getMovie, getMovieVideos, movieId, open, refMovieTrailers])
+
+	const handleClose = () => {
+		refMovieTrailers.current = []
+		setMovie(() => {})
+		setMovieTrailers(() => [])
+		onClose()
+	}
 
 	return createPortal(
 		<>
-			<div className={open ? "modal-movie visible" : "modal-movie"}>
-				<div className='layer' onClick={onClose}></div>
-				<div className='modal-content'>
-					<div className='container'>
-						{/* need to fix CORS or something... */}
-						<iframe width='1080' height='620' title='avengers' src={movie} allowFullScreen></iframe>
+			{movie && (
+				<div className={open ? "modal-movie visible" : "modal-movie"}>
+					<div className='layer' onClick={handleClose}></div>
+					<div className='modal-content'>
+						<div className='container'>
+							<div className='title'>{movie.title}</div>
+							{/* need to fix CORS or something... */}
+							<iframe
+								width='1080'
+								height='620'
+								title='avengers'
+								src={movieTrailers}
+								allowFullScreen></iframe>
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</>,
 		document.getElementById("portal")
 	)
