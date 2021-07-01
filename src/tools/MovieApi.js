fabc6955546c180212df8c5dad06e47b69d11608
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-07-01 18:51:03 +0700

    add MovieRecommandations.

--- src/tools/MovieApi.js
+++ src/tools/MovieApi.js
@@ -60,3 +60,17 @@ export const MovieCredits = async id => {
 	}
 	return
 }
+
+export const MovieRecommandations = async id => {
+	if (id) {
+		baseUrl = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=f363fbafab56237920b96af2c295f5e1`
+		console.log(baseUrl)
+		try {
+			const response = await fetch(baseUrl)
+			return handleResponse(response)
+		} catch (error) {
+			return handleError(error)
+		}
+	}
+	return
+}
